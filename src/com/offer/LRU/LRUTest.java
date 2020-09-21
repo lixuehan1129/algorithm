0587949d76f9906d7f5888de8d2e7de97dac9638
lixuehan <17888836862>
2020-09-21 10:24:12 +0800

    9.21

--- src/com/offer/LRU/LRUTest.java
+++ src/com/offer/LRU/LRUTest.java
@@ -11,11 +11,71 @@ public class LRUTest {
     }
 }
 class LRUCache{
+    static class Node{
+        int key, value;
+        Node pre, next;
+        public Node(int key, int value){
+            this.key = key;
+            this.value = value;
+        }
+    }
+    int capacity;
+    Map<Integer, Node> map;
+    Node head = new Node(-1, -1);
+    Node tail = new Node(-1, -1);
+
+    public LRUCache(int capacity){
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+        head.next = tail;
+        tail.pre = head;
+    }
+
+    public void set(int key, int value){
+        //判断是否存在。如果存在将节点放到头部
+        if(get(key) != -1){
+            map.get(key).value = value;
+        }else{
+            //更新第三个节点头部信息，将第二个节点信息替换为第三个节点
+            if(map.size() == capacity){
+                int headKey = head.next.key;
+                head.next.next.pre = head;
+                head.next = head.next.next;
+                map.remove(headKey);
+            }
+            Node node = new Node(key, value);
+            map.put(key, node);
+            toTail(node);
+        }
+    }
+
+    public int get(int key){
+        if(map.containsKey(key)){
+            //取出该结点
+            Node node = map.get(key);
+            node.pre.next = node.next;
+            node.next.pre = node.pre;
+            toTail(node);
+            return node.value;
+        }
+        return -1;
+    }
+
+    //将节点放置到尾部
+    public void toTail(Node node){
+        node.pre = tail.pre;
+        node.next = tail;
+        tail.pre.next = node;
+        tail.pre = node;
+    }
+}
+
+class LRUCacheLinked{
     private int capacity;
     private Map<Integer,Integer> cache;
     LinkedList<Integer> list;
 
-    public LRUCache(int capacity) {
+    public LRUCacheLinked(int capacity) {
         this.capacity = capacity;
         this.cache = new HashMap<>();
         this.list = new LinkedList<>();
