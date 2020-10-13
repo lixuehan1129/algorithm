77f27c67b84af99195e05235ded9d4719dc81998
lixuehan <17888836862>
2020-10-13 10:37:35 +0800

    10.131

--- src/com/offer/链表/ReverseList.java
+++ src/com/offer/链表/ReverseList.java
@@ -168,6 +168,89 @@ class Solution {
         }
     }
 
+    class 插入排序{
+        public ListNode insertionSortList(ListNode head) {
+            if (head==null ||head.next==null)
+                return head;
+            ListNode pre=head, cur=head.next;           //使用前驱节点pre便于后续节点的删除操作
+            ListNode dummy=new ListNode(0);         //建立一个头结点，便于链表的插入
+            dummy.next=head;
+            while (cur!=null){
+                if (pre.val<cur.val){                   //前后节点已经有序，无需重排
+                    pre=cur;
+                    cur=cur.next;
+                }
+                else {
+                    ListNode p=dummy;
+                    while (p.next!=cur &&p.next.val<cur.val)
+                        p=p.next;
+                    pre.next=cur.next;         //删除当前节点
+                    cur.next=p.next;          //将当前节点连接到对应位置
+                    p.next=cur;
+                    cur=pre.next;
+                }
+            }
+            return dummy.next;
+        }
+    }
+
+    class 归并排序{
+        public ListNode sortList(ListNode head) {
+            // 1、递归结束条件
+            if (head == null || head.next == null) {
+                return head;
+            }
+
+            // 2、找到链表中间节点并断开链表 & 递归下探
+            ListNode midNode = middleNode(head);
+            ListNode rightHead = midNode.next;
+            midNode.next = null;
+
+            ListNode left = sortList(head);
+            ListNode right = sortList(rightHead);
+
+            // 3、当前层业务操作（合并有序链表）
+            return mergeTwoLists(left, right);
+        }
+
+        //  找到链表中间节点（876. 链表的中间结点）
+        private ListNode middleNode(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            }
+            ListNode slow = head;
+            ListNode fast = head.next.next;
+
+            while (fast != null && fast.next != null) {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+
+        // 合并两个有序链表（21. 合并两个有序链表）
+        private ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+            ListNode sentry = new ListNode(-1);
+            ListNode curr = sentry;
+
+            while(l1 != null && l2 != null) {
+                if(l1.val < l2.val) {
+                    curr.next = l1;
+                    l1 = l1.next;
+                } else {
+                    curr.next = l2;
+                    l2 = l2.next;
+                }
+
+                curr = curr.next;
+            }
+
+            curr.next = l1 != null ? l1 : l2;
+            return sentry.next;
+        }
+    }
+
 }
 
 /**
